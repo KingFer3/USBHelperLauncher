025ae6a35fdb3411c46891e75ee8c44e060fbff5
FailedShack <failedshack@gmail.com>
2020-05-04 01:10:26 +0200

    Implement simple MOTD

--- USBHelperLauncher/Program.cs
+++ USBHelperLauncher/Program.cs
@@ -120,6 +120,18 @@ namespace USBHelperLauncher
                 Settings.Save();
             }
 
+            try
+            {
+                MOTD.DisplayIfNeeded(Locale.ChosenLocale);
+            }
+            catch (WebException e)
+            {
+                if (e.Response == null)
+                {
+                    Logger.WriteLine("Could not load message of the day: {0}", e.Message);
+                }
+            }
+
             var certs = new DirectoryInfo("certs");
             if (certs.Exists)
             {
